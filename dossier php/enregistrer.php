184fc6061e4da90947deba915ad4c457b5af6510
jaelle1 <kientiono1254@gmail.com>
2022-04-23 20:39:49 +0000

    FIRST COMMIT

--- dossier php/enregistrer.php
+++ dossier php/enregistrer.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style.css">
-    <title>enregistrer</title>
+    <title>Enregistrer</title>
 </head>
 <body>
 <div class="container">
